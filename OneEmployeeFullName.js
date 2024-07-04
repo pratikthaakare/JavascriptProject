4e776a57a0e694ef2cc2949434edd4f0b624b6aa
Pratik G. Thakare <pratikthaakare@gmail.com>
2024-07-04 12:49:15 +0530

    Create OneEmployeeFullName.js
    Code for Only one Employee Full Name

---
+++ OneEmployeeFullName.js
@@ -0,0 +1,10 @@
+//This file is created to test my begineer level of code in JS
+function fullname(){
+    let firstname = 'Jane'
+    let middlename = 'Father'
+    let lastname = 'Doe'
+    let employeeName = firstname +" "+  middlename +" "+ lastname
+    console.log(employeeName)
+}
+
+fullname()
