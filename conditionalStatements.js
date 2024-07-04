435e4e7d554a514265c41915b8c93cb8615ab3b8
pratikthaakare <pratikthaakare@gmail.com>
2024-07-04 15:56:55 +0530

    added conditionalStatements.js file

---
+++ conditionalStatements.js
@@ -0,0 +1,8 @@
+function ifelse(){
+    let temperature = prompt("Please Enter current temperature in degree celsius: ");
+    if (temperature > 30){
+        console.log('Weather is most likely to be hot today. Carry a water bottle with you. ');
+    }else(console.log("The weather is colder today than usual, Carry a jacket today."))
+}
+
+ifelse()
